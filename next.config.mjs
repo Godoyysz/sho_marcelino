faee17002437707fc21af73bd4f8727ef3d08e80
Godoyysz <131817536+Godoyysz@users.noreply.github.com>
2024-06-19 14:07:16 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "fakestoreapi.com",
+            "br.pinterest.com",
+            "i.pinimg.com",
+            "backiee.com",
+            "scontent.whatsapp.net",
+            "www.whatsapp.com"
+        ]
+    }
+};
+
+export default nextConfig;
